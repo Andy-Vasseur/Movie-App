1174ee3b69dda50754fecc123e7a8d44ea9c9be9
Andy-Vasseur <beaugosswwe@live.fr>
2023-04-05 02:08:23 +0200

    .

---
+++ src/pages/contact.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import emailjs from 'emailjs-com';
+import Navigation from '@/components/Navigation';
+
+export default function Contact() {
+
+    const [messageSended, setMessageSended] = React.useState(null);
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm(`${process.env.NEXT_PUBLIC_SERVICE_ID}`, `${process.env.NEXT_PUBLIC_TEMPLATE_ID}`, e.target, `${process.env.NEXT_PUBLIC_PUBLIC_ID}`)
+            .then((result) => {
+                console.log(`[${result.text}] Message envoyé !`);
+                setMessageSended(true);
+                e.target.reset();
+            }
+                , (error) => {
+                    console.log(error.text);
+                    setMessageSended(false);
+                });
+    }
+
+    const messageSuccess = () => {
+        return <p>Message sended !</p>
+    }
+
+    const messageError = () => {
+        return <p>Message not sended !</p>
+    }
+
+    return (
+        <div className="Contact">
+            <Navigation />
+
+            <div className="Form">
+                <form id='contact' className="form-container" onSubmit={sendEmail}>
+                    <h2>Contact</h2>
+                    <p>
+                        {messageSended === true ? messageSuccess() : messageSended === false ? messageError() : null}
+                    </p>
+
+                    <label htmlFor="name">Nom complet</label>
+                    <input type="text" name="from_name" placeholder='Your full name...' required />
+
+                    <label htmlFor="mail">E-mail</label>
+                    <input type="email" name="from_email" placeholder='Your email...' required />
+
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" placeholder='Your message...' required />
+
+                    <input type="submit" value="Envoyer" />
+                </form>
+            </div>
+        </div>
+    );
+
+}
--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -1,40 +1,99 @@
-import Navigation from '@/components/Navigation';
-import axios from 'axios';
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import axios from 'axios';
+import Select from 'react-select';
+import Navigation from '@/components/Navigation';
+import { useState, useEffect } from 'react';
+import ScrollToTop from 'react-scroll-to-top';
 
 const Movies = () => {
-
-    const [trending, setTrending] = React.useState([]);
-
-    React.useEffect(() => {
-        axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=1455fc9a99d80cb88d2712a9f4ff900f')
+    // Calls API
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        axios
+            .get(
+                'https://api.themoviedb.org/3/discover/movie?api_key=1455fc9a99d80cb88d2712a9f4ff900f&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+            )
             .then((res) => {
-                setTrending(res.data.results);
+                setMovies(res.data.results);
             })
             .catch((err) => {
                 console.log(err);
             });
     }, []);
 
+    // React Select
+    const [selectedOption, setSelectedOption] = useState(null);
+    const options = [
+        { value: 'Choose a filter...', label: 'Choose a filter...', isDisabled: true },
+        { value: 'all', label: 'All movies & series' },
+        { value: 'trending', label: 'Trending' },
+        { value: 'top_rated', label: 'Top Rated' },
+    ];
+
+    // Filter movies by selected option
+    const filteredMovies = selectedOption
+        ? movies.filter((movie) => {
+            if (selectedOption.value === 'trending') {
+                return movie.popularity > 0;
+            } else if (selectedOption.value === 'top_rated') {
+                return movie.vote_average >= 8;
+            } else {
+                return true;
+            }
+        })
+        : movies;
+
     return (
         <div className='Movies'>
             <Navigation />
             <h1>Movies</h1>
 
-            <ul className="Movies-trending">
-                {trending.map((movie) => (
+            <Select
+                styles={{
+                    control: (provided) => ({
+                        ...provided,
+                        width: '300px',
+                        margin: '0 auto',
+                        marginBottom: '3rem',
+                        border: 'none',
+                        borderRadius: '8px',
+                    }),
+                }}
+                placeholder='Choose a filter...'
+                value={selectedOption}
+                onChange={setSelectedOption}
+                options={options}
+            />
+
+            <ul className='Movies-trending'>
+                {filteredMovies.map((movie) => (
                     <Link href={`/movies/${movie.id}`} className='Movie-card' key={movie.id}>
                         <Image
                             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                            alt={movie.title}
+                            alt={movie.id}
                             width={300}
                             height={450}
                         />
                     </Link>
                 ))}
             </ul>
+            <ScrollToTop
+                smooth
+                top={400}
+                width='28px'
+                height='28px'
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: '40%',
+                    width: '50px',
+                    height: '50px',
+                }}
+                className='icon-hover'
+            />
         </div>
     );
 };
